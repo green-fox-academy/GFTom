7f349a5dbdd544b039755948376f5a257a2411d8
Tamas Banyai <banyai.tamas23@gmail.com>
2018-08-03 11:58:58 +0200

    feat(Swap): finished task

--- week-01/day-03/FavoriteNumber/src/FavoriteNumber.java
+++ week-01/day-03/FavoriteNumber/src/FavoriteNumber.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class FavoriteNumber {
+  public static void main(String[] args) {
+    int a = 8;
+    System.out.println( " My favorite number is " + a);
+    // Store your favorite number in a variable (as a number)
+    // And print it like this: "My favorite number is: 8"
+  }
 }
