61ca2308796a5d3167230f5528dfebc1e01df81b
Tamas Banyai <banyai.tamas23@gmail.com>
2018-08-03 13:10:58 +0200

    feat(DefineBasicInfo): task is completed

---
+++ week-01/day-03/DefineBasicInfo/src/DefineBasicInfo.java
@@ -0,0 +1,12 @@
+public class DefineBasicInfo {
+  public static void main(String[] args) {
+    String mynameis = "Tamas Banyai";
+    System.out.println(mynameis);
+    int a = 33;
+    System.out.println(a);
+    double b = 1.78;
+    System.out.println(b);
+    boolean Ifimarried = true;
+    System.out.println(Ifimarried);
+  }
+}
