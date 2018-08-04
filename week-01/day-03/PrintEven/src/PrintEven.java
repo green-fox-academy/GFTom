1ecc973c7bd3e5bde8cc1f6732336c50f207c8a9
Tamas Banyai <banyai.tamas23@gmail.com>
2018-08-04 13:36:01 +0200

    feat(PrintEven): task complete

---
+++ week-01/day-03/PrintEven/src/PrintEven.java
@@ -0,0 +1,15 @@
+public class PrintEven {
+  // Create a program that prints all the even numbers between 0 and 500
+  public static void main(String[] args) {
+
+  int a = 0;
+  while(a < 501){
+  if(a % 2 == 0){
+    System.out.println(a);
+  }
+    a = a + 1;
+  }
+
+
+  }
+}
