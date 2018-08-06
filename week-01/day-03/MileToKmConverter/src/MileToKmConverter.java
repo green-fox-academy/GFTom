f109ca75f19faf81862032c88c24f5bc610f44d8
Tamas Banyai <banyai.tamas23@gmail.com>
2018-08-06 15:51:24 +0200

    feat(Doubling): task complete

---
+++ week-01/day-03/MileToKmConverter/src/MileToKmConverter.java
@@ -0,0 +1,9 @@
+public class MileToKmConverter {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    double a = scanner.nextLine();
+    System.out.println(userInput1);
+    // Write a program that asks for an integer that is a distance in kilometers,
+    // then it converts that value to miles and prints it
+  }
+}
