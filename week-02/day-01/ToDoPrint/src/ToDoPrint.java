cef50cef9a80e38f9a7c4e808ee8150177b85e94
Tamas Banyai <banyai.tamas23@gmail.com>
2018-08-07 15:27:54 +0200

    feat(ToDoList): task complete

---
+++ week-02/day-01/ToDoPrint/src/ToDoPrint.java
@@ -0,0 +1,57 @@
+public class ToDoPrint {
+  public static void main(String... args){
+    String todoText = " My todo:\n" + " - Buy milk\n" + " - Download games\n" + " \t- Diabolo";
+    //String mt = " My todo";
+    //String games = " - Download games";
+    //String dia = " - Diabolo";
+    // Add "My todo:" to the beginning of the todoText
+    // Add " - Download games" to the end of the todoText
+    // Add " - Diablo" to the end of the todoText but with indention
+
+    // Expected outpt:
+
+    // My todo:
+    //  - Buy milk
+    //  - Download games
+    //      - Diablo
+
+    System.out.println(todoText);
+    //String quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
+    //String quotenew = " always takes no longer";
+    //StringBuilder quotetemp = new StringBuilder(quote);
+    //quotetemp.insert(20, quotenew);
+    //quote = quotetemp.toString();
+    // When saving this quote a disk error has occured. Please fix it.
+    // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
+    // Using pieces of the quote variable (instead of just redefining the string)
+
+    //System.out.println(quote);
+  //}
+  //public class UrlFixer {
+  //public static void main(String... args){
+  //String url = "https//www.reddit.com/r/nevertellmethebots";
+  //url = (String) url.replace("bots","odds" );
+  //String insert = ":";
+  //StringBuilder str = new StringBuilder(url);
+  //str.insert(5, insert);
+  //url = str.toString();
+  // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+  // Also, the URL is missing a crutial component, find out what it is and insert it too!
+  // Try to solve it more than once using different String functions!
+
+  //System.out.println(url);
+}
+//public class simplereplace {
+//public static void main(String... args){
+//String example = "In a dishwasher far far away";
+//example = example.replace("dishwasher", "galaxy");
+// I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
+
+//System.out.println(example);
+//}
+//}
+//}
+//}
+}
