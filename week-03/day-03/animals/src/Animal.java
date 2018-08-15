a466ccb76a2cb42db3fd4567b8762e6b0d62defc
Tamas Banyai <banyai.tamas23@gmail.com>
2018-08-15 18:57:29 +0200

    feat(animals): task completed

---
+++ week-03/day-03/animals/src/Animal.java
@@ -0,0 +1,29 @@
+public class Animal {
+
+    int hunger;
+    int thirst;
+
+    public Animal() {
+        this(50,50);
+    }
+
+    public Animal(int hunger, int thirst) {
+        this.hunger = hunger;
+        this.thirst = thirst;
+    }
+
+    public void eat(){
+        hunger -= 1;
+    }
+    
+    public void drink(){
+        thirst -= 1;
+    }
+
+    public void play (){
+        hunger += 1;
+        thirst += 1;
+    }
+
+}
+
