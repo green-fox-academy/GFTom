c027a4c03028b361c22ee1ddbca764f67b2a9487
Tamas Banyai <banyai.tamas23@gmail.com>
2018-08-16 11:51:52 +0200

    feat(Sharpie): task finished

---
+++ week-03/day-03/Sharpie/src/Sharpie.java
@@ -0,0 +1,16 @@
+public class Sharpie {
+
+    String color;
+    float width;
+    float inkAmount;
+
+    public Sharpie (String color, float width, float inkAmount){
+        this.color = color;
+        this.width = width;
+        this.inkAmount = 100;
+    }
+
+    public void use(){
+        inkAmount = inkAmount - 5;
+    }
+}
