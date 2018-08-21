8e74062f8b25d7907b5344b05bdc998b22e00bc7
Tamas Banyai <banyai.tamas23@gmail.com>
2018-08-21 16:33:42 +0200

    feat(GreenFoxInheritance): Mentor and Sponsor classes are created

---
+++ week-04/day-01/GreenFoxInheritance/src/Mentor.java
@@ -0,0 +1,19 @@
+public class Mentor extends Person {
+
+    private String level;
+
+    public Mentor(String name, int age, String gender) {
+        super(name, age, gender);
+    }
+
+    public Mentor() {
+    }
+
+    public void introduce() {
+        System.out.println("Hi, I'm " + name + ", a " + age + "year old" + gender + level + " mentor.");
+    }
+
+    public void getGoal() {
+        System.out.println("Educate brilliant junior software developers!");
+    }
+}
