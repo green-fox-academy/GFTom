b99d34088eeea665dec0226db45dd6876bb558d7
Tamas Banyai <banyai.tamas23@gmail.com>
2018-08-27 18:15:29 +0200

    feat(Zoo): Classes started

---
+++ week-04/day-02/Zoo/src/Animals.java
@@ -0,0 +1,14 @@
+public abstract class Animals {
+
+    private String name;
+    private int age;
+    private String gender;
+
+    public  void getName (){
+
+    }
+
+    public  void breed (){
+
+    }
+}
