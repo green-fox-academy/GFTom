848b95c84215e9c5195b66120fb52ed873f3da28
Tamas Banyai <banyai.tamas23@gmail.com>
2018-09-13 14:39:12 +0200

    feat(AntiDiagonal): Task finished

---
+++ week-05/day-01/AntiDiagonal/src/AntiDiagonal.java
@@ -0,0 +1,43 @@
+public class AntiDiagonal {
+
+    public static void main(String[] args) {
+        int[][] twoDimensionMatrix = new int[][]{{1,1,1},{2,2,2},{3,3,3}};
+
+        System.out.println(getAntiDiagonalAvg(twoDimensionMatrix));
+
+    }
+//int[][] twoDimensionMatrix = new int[][]{{1,1,1},{2,2,2},{3,3,3}};
+    public static double getAntiDiagonalAvg(int[][] twoDimensionMatrix){
+        int sumAntiDiagonal = 0;
+        int countAntidiagonalElements = twoDimensionMatrix.length;
+        for (int i = 0; i < twoDimensionMatrix.length; i++) {
+            for (int j = 0; j < twoDimensionMatrix[i].length; j++) {
+                if(j == twoDimensionMatrix[i].length - i - 1){
+                    sumAntiDiagonal = sumAntiDiagonal + twoDimensionMatrix[i][j];
+                }
+            }
+        }
+        return sumAntiDiagonal/countAntidiagonalElements;
+    }
+    /**
+     * Create and test a method called `getAntiDiagonalAvg` that takes a multidimensional array (only integers)
+     * which represents a square matrix, as a parameter and returns the average of the numbers in the anti diagonal.
+     *  - write test for 2 different scenarios
+     *
+     * Example cases:
+     * [
+     *   [1, 2, 3],
+     *   [3, 4, 6],
+     *   [5, 2, 5]
+     * ]
+     * Should return 4, because (3 + 4 + 5) / 3 = 4.
+     *
+     * [
+     *   [3, 5, 11, -2],
+     *   [3, 1, 7, 4],
+     *   [5, 0, 2, 9],
+     *   [21, 7, 8, 2]
+     * ]
+     * Should return 6.5, because (-2 + 7 + 0 + 21) / 4 = 6.5.
+     */
+}
