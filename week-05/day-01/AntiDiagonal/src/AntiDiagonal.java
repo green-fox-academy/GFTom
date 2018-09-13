5b6aa7d16b9d7feb23b97ef8bcae2908237ee99c
Tamas Banyai <banyai.tamas23@gmail.com>
2018-09-13 15:19:30 +0200

    feat(AntiDiagonal): two additional functions created

--- week-05/day-01/AntiDiagonal/src/AntiDiagonal.java
+++ week-05/day-01/AntiDiagonal/src/AntiDiagonal.java
@@ -1,9 +1,11 @@
 public class AntiDiagonal {
 
     public static void main(String[] args) {
-        int[][] twoDimensionMatrix = new int[][]{{1,1,1},{2,2,2},{3,3,3}};
+        int[][] twoDimensionMatrix = new int[][]{{1,8,10},{5,6,2},{4,3,9}};
 
         System.out.println(getAntiDiagonalAvg(twoDimensionMatrix));
+        System.out.println(getDiagonalAvg(twoDimensionMatrix));
+        System.out.println(getMaxElementsAvg(twoDimensionMatrix));
 
     }
 //int[][] twoDimensionMatrix = new int[][]{{1,1,1},{2,2,2},{3,3,3}};
@@ -18,6 +20,29 @@ public class AntiDiagonal {
             }
         }
         return sumAntiDiagonal/countAntidiagonalElements;
+    }
+
+    public static double getDiagonalAvg(int[][] twoDimensionMatrix){
+        int sumDiagonal =0;
+        int countDiagonalElements = twoDimensionMatrix.length;
+        for (int i = 0; i < twoDimensionMatrix.length; i++) {
+            sumDiagonal = sumDiagonal + twoDimensionMatrix[i][i];
+        }
+        return sumDiagonal/countDiagonalElements;
+    }
+
+    public static double getMaxElementsAvg(int[][] twoDimensionMatrix){
+        int sumMaxElements = 0;
+        for (int i = 0; i < twoDimensionMatrix.length; i++) {
+            int tempMaxElement = 0;
+            for (int j = 0; j < twoDimensionMatrix[i].length; j++) {
+                if(twoDimensionMatrix[i][j] > tempMaxElement){
+                    tempMaxElement = twoDimensionMatrix[i][j];
+                }
+            }
+            sumMaxElements = sumMaxElements + tempMaxElement;
+        }
+        return sumMaxElements/twoDimensionMatrix.length;
     }
     /**
      * Create and test a method called `getAntiDiagonalAvg` that takes a multidimensional array (only integers)
